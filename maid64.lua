5acebabb644f39e84b950a85b97823d6cce1039f
Jesse Paroz <jparoz@users.noreply.github.com>
2016-11-01 13:50:50 +1000

    Cleanup code and formatting
    Use consistent indentation
    
    Use consistent function declaration syntax
    
    Removed redundant function parameters in calls to:
    - `love.graphics.draw`
    - `maid64.canvas:setFilter`
    - `image:setFilter`
    
    Maintain line length less than 80

--- maid64.lua
+++ maid64.lua
@@ -2,32 +2,35 @@
 --        version 1.5        --
 --        MIT license        --
 -- Copyright (c) 2016 adekto --
+
 maid64 = {mouse = {}}
+
 function maid64.setup(x,y)
     maid64.sizeX = x or 64
     maid64.sizeY = y or maid64.sizeX
     if x < (y or 0) then
-      maid64.scaler = love.graphics.getHeight() / maid64.sizeY
+        maid64.scaler = love.graphics.getHeight() / maid64.sizeY
     else
-      maid64.scaler = love.graphics.getWidth() / maid64.sizeX
+        maid64.scaler = love.graphics.getWidth() / maid64.sizeX
     end
-    maid64.x = love.graphics.getWidth()/2-(maid64.scaler*(maid64.sizeX/2))
-    maid64.y = love.graphics.getHeight()/2-(maid64.scaler*(maid64.sizeY/2))
+    maid64.x = love.graphics.getWidth()/2 - (maid64.scaler * (maid64.sizeX/2))
+    maid64.y = love.graphics.getHeight()/2 - (maid64.scaler * (maid64.sizeY/2))
     maid64.canvas = love.graphics.newCanvas(maid64.sizeX, maid64.sizeY)
-    maid64.canvas:setFilter("nearest","nearest")
+    maid64.canvas:setFilter("nearest")
 
-    maid64.resize(love.graphics.getDimensions()) -- run resize so the sceen is reset corectly
+    -- run resize so the sceen is reset corectly
+    maid64.resize(love.graphics.getDimensions())
 end
 
-maid64.start = function ()
+function maid64.start()
+    love.graphics.setCanvas(maid64.canvas)
+    love.graphics.clear()
+end
 
-      love.graphics.setCanvas(maid64.canvas)
-      love.graphics.clear()
-    end
-maid64.finish = function ()
-       love.graphics.setCanvas()
-       --love.graphics.setShader() --used only for special shaders
-       love.graphics.draw(maid64.canvas, maid64.x,maid64.y,0,maid64.scaler,maid64.scaler)
+function maid64.finish()
+    love.graphics.setCanvas()
+    --love.graphics.setShader() --used only for special shaders
+    love.graphics.draw(maid64.canvas, maid64.x, maid64.y, 0, maid64.scaler)
 end
 
 function maid64.resize(w, h)
@@ -36,32 +39,41 @@ function maid64.resize(w, h)
     else
         maid64.scaler = w / maid64.sizeX
     end
-    maid64.x = w/2-(maid64.scaler*(maid64.sizeX/2))
-    maid64.y = h/2-(maid64.scaler*(maid64.sizeY/2))
+    maid64.x = w/2 - (maid64.scaler * (maid64.sizeX/2))
+    maid64.y = h/2 - (maid64.scaler * (maid64.sizeY/2))
 end
 
 function maid64.mouse.getPosition()
-  local my = math.floor( (love.mouse.getY()-maid64.y)/(maid64.scaler *maid64.sizeX)*maid64.sizeX)
-  local mx = math.floor( (love.mouse.getX()-maid64.x)/(maid64.scaler *maid64.sizeY)*maid64.sizeY)
-  return mx,my
+    local mx = math.floor((love.mouse.getX() - maid64.x) /
+        (maid64.scaler * maid64.sizeY) * maid64.sizeY)
+    local my = math.floor((love.mouse.getY() - maid64.y) /
+        (maid64.scaler * maid64.sizeX) * maid64.sizeX)
+    return mx, my
 end
+
 function maid64.mouse.getX()
-  return math.floor( (love.mouse.getX()-maid64.x)/(maid64.scaler *maid64.sizeX)*maid64.sizeX)
+    return math.floor((love.mouse.getX() - maid64.x) /
+        (maid64.scaler * maid64.sizeX) * maid64.sizeX)
 end
+
 function maid64.mouse.getY()
-  return math.floor( (love.mouse.getY()-maid64.y)/(maid64.scaler *maid64.sizeY)*maid64.sizeY)
+    return math.floor((love.mouse.getY() - maid64.y) /
+        (maid64.scaler * maid64.sizeY) * maid64.sizeY)
 end
 
 function maid64.newImage(source)
-  local image = love.graphics.newImage(source)
-  image:setFilter("nearest","nearest")
-  return image
+    local image = love.graphics.newImage(source)
+    image:setFilter("nearest")
+    return image
 end
 
 function maid64.newTileSet(image, x, y)
-  local quad = {x = x, y = y}
-  for i=0,(image:getWidth()/x)*(image:getHeight()/y)-1 do
-    quad[i] = love.graphics.newQuad((i%(image:getWidth()/x))*x, math.floor(i/(image:getWidth()/x))*y, x, y, image:getDimensions())
-  end
-  return quad
+    local quad = {x = x, y = y}
+    for i=0, (image:getWidth()/x) * (image:getHeight()/y) - 1 do
+        quad[i] = love.graphics.newQuad(
+            (i % (image:getWidth()/x)) * x,
+            math.floor(i/(image:getWidth()/x))*y,
+            x, y, image:getDimensions())
+    end
+    return quad
 end
